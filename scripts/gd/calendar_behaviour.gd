e0b0ad3493fad5fd955d1c57348b40d87536faf7
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-07-12 10:38:42 +0200

    Add calendar scene and behaviour script
    Introduced a new Calendar scene composed of Day nodes, each with collision detection. Added a CalendarBehaviour script to handle Area2D body enter/exit events, setting up the structure for calendar interactions.

---
+++ scripts/gd/calendar_behaviour.gd
@@ -0,0 +1,19 @@
+class_name CalendarBehaviour extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for child in get_children().filter(func c(): return Area2D):
+		child = child as Area2D
+		child.body_entered.connect(_on_area_2d_body_entered).bind(child)
+		child.body_exited.connect(_on_area_2d_body_exited).bind(child)
+
+func _on_area_2d_body_entered(area):
+	pass
+
+func _on_area_2d_body_exited(area):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
