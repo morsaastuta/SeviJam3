1ef1d1517d914124c24fe8214aada05ff51c0333
Morsa <morsaastuta@gmail.com>
2025-07-12 13:15:20 +0200

    a

--- scripts/gd/global.gd
+++ scripts/gd/global.gd
@@ -1,11 +1,8 @@
 extends Node
 
 signal grabbed(magnet: MagnetBehaviour)
-signal dropped
+signal dropped(magnet: MagnetBehaviour)
 
 @onready var fridge: FridgeBehaviour = get_node("Fridge")
 @onready var fridge_stickers: FridgeBehaviour = get_node("Fridge/GroupStickers")
 @onready var fridge_calendar: FridgeBehaviour = get_node("Fridge/GroupCalendar")
-@onready var fridge_tray: 
-	
-signal dropped(magnet: MagnetBehaviour)
