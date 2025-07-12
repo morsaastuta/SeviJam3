47c2b1bf7acccd350e7d070458afe6d62072422c
Morsa <morsaastuta@gmail.com>
2025-07-12 13:11:13 +0200

    a

--- scripts/gd/fridge_behaviour.gd
+++ scripts/gd/fridge_behaviour.gd
@@ -1,5 +1,6 @@
 class_name FridgeBehaviour extends Node2D
 
+@export var screen: CanvasModulate
 @export var calendar_current: CalendarBehaviour
 var calendar_next: CalendarBehaviour
 
@@ -7,3 +8,11 @@ func set_level(calendar_next: CalendarBehaviour) -> void:
 	self.add_child(calendar_next)
 	# esperar a que cargue
 	# animar calendar_current
+
+func show_stickers(on: bool) -> void:
+	var tween: Tween = create_tween()
+	if on:
+		tween.tween_property(screen, "color:a", 0.6, 0.5)
+		
+	else:
+		tween.tween_property(screen, "color:a", 0, 0.5)
--- scripts/gd/sticker_behaviour.gd
+++ scripts/gd/sticker_behaviour.gd
@@ -1,4 +1,4 @@
-class_name MagnetBehaviour extends RigidBody2D
+class_name StickerBehaviour extends Sprite2D
 
 var held: bool = false
 
@@ -10,20 +10,15 @@ func _on_input_event(viewport, event, shape_idx) -> void:
 
 func _physics_process(delta) -> void:
 	if not held: return
-	print("is held")
 	global_position.x = lerp(global_position.x, get_global_mouse_position().x, 0.35)
 	global_position.y = lerp(global_position.y, get_global_mouse_position().y, 0.35)
 
 func grab() -> void:
-	print("grab")
-	set_collision_layer_value(1, false)
-	set_collision_mask_value(1, false)
+	reparent(Global.fridge)
 	held = true
 	Global.grabbed.emit(self)
 	
 func drop() -> void:
-	print("drop")
-	set_collision_layer_value(1, true)
-	set_collision_mask_value(1, true)
+	reparent(Global.fridge_stickers)
 	held = false
 	Global.dropped.emit(self)
