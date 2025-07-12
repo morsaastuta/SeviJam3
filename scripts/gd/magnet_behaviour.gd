0029eea9c3793b3c13dfca50e507663b5283f65e
Morsa <morsaastuta@gmail.com>
2025-07-12 18:15:10 +0200

    a

--- scripts/gd/magnet_behaviour.gd
+++ scripts/gd/magnet_behaviour.gd
@@ -7,16 +7,14 @@ var held: bool = false
 func _on_input_event(viewport, event, shape_idx) -> void:
 	if event is InputEventMouseButton:
 		if !Global.fridge.calendar.held and !held and event.is_pressed(): grab()
-		elif Global.fridge.calendar.held and held  and !event.is_pressed(): drop()
+		elif Global.fridge.calendar.held and held and !event.is_pressed(): drop()
 
 func _physics_process(delta) -> void:
 	if !Global.fridge.calendar.held || !held: return
-	print("is held")
 	global_position.x = lerp(global_position.x, get_global_mouse_position().x, 0.35)
 	global_position.y = lerp(global_position.y, get_global_mouse_position().y, 0.35)
 
 func grab() -> void:
-	print("grab")
 	origin = global_position
 	set_collision_layer_value(1, false)
 	set_collision_mask_value(1, false)
@@ -25,7 +23,6 @@ func grab() -> void:
 	Global.grabbed.emit(self)
 	
 func drop() -> void:
-	print("drop")
 	set_collision_layer_value(1, true)
 	set_collision_mask_value(1, true)
 	held = false
