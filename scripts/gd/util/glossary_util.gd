646bd32034dc5e7e5b840bebb3135ead07bceb73
Morsa <morsaastuta@gmail.com>
2025-07-12 20:42:31 +0200

    a

--- scripts/gd/util/glossary_util.gd
+++ scripts/gd/util/glossary_util.gd
@@ -13,3 +13,12 @@ const WIND: StringName = &"wind"
 
 # Enums
 enum Effect {NONE, RAIN, SUN, STORM, RAINBOW, WIND}
+
+func effect_by_name(effect_name: String) -> Effect:
+	match effect_name:
+		RAIN: return Effect.RAIN
+		SUN: return Effect.SUN
+		STORM: return Effect.STORM
+		RAINBOW: return Effect.RAINBOW
+		WIND: return Effect.WIND
+		_: return Effect.NONE
