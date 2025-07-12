7773f5c98328866934e7dfc283b0a1985e0cb879
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-07-12 19:29:18 +0200

    Add rules export and string parsing to calendar
    Introduces an exported 'rules' array to CalendarBehaviour and adds a string_to_calendar function for parsing rule strings. Also updates the calendar scene to initialize the 'dic' property with sample data.

--- scripts/gd/calendar_behaviour.gd
+++ scripts/gd/calendar_behaviour.gd
@@ -5,6 +5,12 @@ class_name CalendarBehaviour extends Node2D
 var selected_magnet: MagnetBehaviour
 var held: bool = false
 
+@export var rules: Array[String]
+
+@export var 
+func _ready() -> void:
+	pass
+
 func _mission_achieved():
 	pass
 
@@ -13,3 +19,15 @@ func _on_area_2d_body_entered(area):
 
 func _sticker_achieved():
 	pass
+
+func string_to_calendar(str:String):
+	var str_array := str.split("_")
+	match str_array[0]:
+		"any":
+			pass
+		"all":
+			pass
+		_:
+			var n:int = str_array[0].to_int()
+			
+	
