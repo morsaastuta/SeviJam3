253db367c61bf84c2709d5abca69134b95948f30
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-07-12 17:50:28 +0200

    Add calendar scene and enhance day behaviour logic
    Introduces a new calendar.tscn scene with multiple day instances and a magnet. Updates day.tscn to include a Sprite2D node. Expands day_behaviour.gd with effect management, new signals, and requirement checking logic. Adjusts Magnet.tscn collision shape radius and adds a new check_day signal to global.gd.

--- scripts/gd/day_behaviour.gd
+++ scripts/gd/day_behaviour.gd
@@ -1,16 +1,44 @@
-extends Area2D
+class_name DayBehaviour extends Area2D
 
 @export var calendar: CalendarBehaviour
 @export var magnet_marker: Marker2D
 var magnet_position: Vector2
 
+@export var sprite: Sprite2D
 @export var no_magnet: bool
+@export var no_apply_drawing: bool
 @export var requirements: Dictionary[StringName, bool] = {
 	
 }
 
 var magnet_hover: MagnetBehaviour
-var magnet_applied: MagnetBehaviour
+var magnet_applied: MagnetBehaviour:
+	get:
+		return magnet_applied
+	set(value):
+		magnet_applied = value
+		if magnet_applied: effect_applied = Effect.RAIN; no_apply_drawing = true
+		else: effect_applied = Effect.NONE; no_apply_drawing = false
+
+enum Effect{
+	NONE,
+	SUN,
+	RAINBOW,
+	RAIN,
+	STORM
+}
+var effect_applied: Effect = Effect.NONE:
+	get:
+		return effect_applied
+	set(value):
+		effect_applied =  value
+		
+		match effect_applied:
+			Effect.NONE: sprite.texture = null
+			Effect.SUN: sprite.texture = null #sun textrure
+			Effect.RAINBOW: sprite.texture = null #rainbow texture
+			Effect.RAIN: sprite.texture = null #rain texture
+			Effect.STORM: sprite.texture = null #storm texture
 
 func _ready() -> void:
 	if not calendar and get_parent() is CalendarBehaviour:
@@ -22,8 +50,10 @@ func _ready() -> void:
 		else: magnet_position = magnet_marker.global_position
 	
 	Global.dropped.connect(_on_magnet_dropped)
+	
+	Global.check_day.connect(_self_check)
 
-func _on_magnet_dropped(magnet: MagnetBehaviour):
+func _on_magnet_dropped(magnet: MagnetBehaviour) -> void:
 	if not magnet_hover: return
 	if not magnet_hover == calendar.selected_magnet: printerr("QUE COÑO?, day_behaviour.gd")
 	
@@ -35,7 +65,7 @@ func _on_magnet_dropped(magnet: MagnetBehaviour):
 	
 	magnet_hover = null; calendar.selected_magnet = null
 
-func _on_magnet_grabbed(magnet: MagnetBehaviour):
+func _on_magnet_grabbed(magnet: MagnetBehaviour) -> void:
 	if not magnet_applied: return
 	if magnet_hover: return
 	
@@ -57,3 +87,18 @@ func _on_body_exited(body: Node2D) -> void:
 	
 	magnet_hover = null
 	calendar.selected_magnet = null
+
+func _self_check() -> void:
+	for key in requirements.keys():
+		match key:
+			ReqName.RAIN:
+				requirements[key] = effect_applied == Effect.RAIN 
+			ReqName.RAINBOW:
+				requirements[key] = effect_applied == Effect.RAINBOW
+			ReqName.STORM:
+				requirements[key] = effect_applied == Effect.STORM
+			ReqName.SUN:
+				requirements[key] = effect_applied == Effect.SUN
+	
+
+		
