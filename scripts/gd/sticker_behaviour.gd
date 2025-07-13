1cdd122d607ca835bd4f986d4011291c906ab05d
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-07-13 14:00:18 +0200

    Update sticker_behaviour.gd

--- scripts/gd/sticker_behaviour.gd
+++ scripts/gd/sticker_behaviour.gd
@@ -3,11 +3,15 @@ class_name StickerBehaviour extends Sprite2D
 @export var area: Area2D
 var held: bool = false
 
+func _ready() -> void:
+	self.set_instance_shader_parameter("hovering",0.0)
+
 func _on_input_event(viewport, event, shape_idx) -> void:
 	if event is InputEventMouseButton:
 		if !Global.fridge.calendar.held && !held && event.is_action_pressed("click"): grab()
 		elif Global.fridge.calendar.held && held && event.is_action_pressed("click"): drop()
 
+	
 func _physics_process(delta) -> void:
 	if !Global.fridge.calendar.held || !held: return
 	global_position.x = lerp(global_position.x, get_global_mouse_position().x, 0.35)
@@ -23,6 +27,7 @@ func drop() -> void:
 	reparent(Global.fridge.group_stickers)
 	held = false
 	Global.fridge.calendar.held = false
+	material = null
 	set_input_pickable(false)
 	Global.fridge.next_level()
 
