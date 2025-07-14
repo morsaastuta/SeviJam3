27354372b15db236620204f87dcaa63828bf1ec9
AlexOde <68616269+AlexOd4@users.noreply.github.com>
2025-07-14 19:44:06 +0200

    Refactor continue button and add press sound effect
    Moved the continue button logic to a new script (continue_button_config.gd) and updated the scene to use it. Added a ButtonSound AudioStreamPlayer node and connected the button's pressed signal to play a sound effect. Also updated export paths and added an Android export preset.

---
+++ scripts/gd/continue_button_config.gd
@@ -0,0 +1,11 @@
+extends Button
+
+func _on_mouse_entered() -> void: create_tween().tween_property(self, "scale", Vector2.ONE*1.10, 0.1)
+
+func _on_mouse_exited() -> void: create_tween().tween_property(self, "scale", Vector2.ONE, 0.1)
+
+func _on_button_down() -> void: create_tween().tween_property(self, "scale", Vector2.ONE*0.9, 0.025)
+
+func _on_button_up() -> void: create_tween().tween_property(self, "scale", Vector2.ONE, 0.05)
+
+func _on_pressed(): %ButtonSound.play()
